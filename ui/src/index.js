08d655d4b1e5bf07d81ff1b373b9b7aa83e2c5fe
Bill Tarr <betarr13@gmail.com>
2018-04-22 20:33:59 -0400

    use semantic ui

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Header } from 'semantic-ui-react';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render((<h1>Hello World!</h1>), document.getElementById('root'));
+ReactDOM.render((<Header size="huge">Hello World!</Header>), document.getElementById('root'));
 registerServiceWorker();
