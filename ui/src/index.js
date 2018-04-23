f12a4d43dc64f4240e34f4fd9fcfeae62d4705a3
Bill Tarr <betarr13@gmail.com>
2018-04-22 20:12:47 -0400

    basic hello world ui

---
+++ ui/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render((<h1>Hello World!</h1>), document.getElementById('root'));
+registerServiceWorker();
